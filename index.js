d8541826b098c5e01a1eb6bf96aa3bb7e8b39836
Eduardo Coello <jeck24@hotmail.com>
2020-11-30 17:04:15 -0600

    fixed questions

--- index.js
+++ index.js
@@ -1,16 +1,56 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
 
 // array of questions for user
-const questions = ['What is your GitHub username?',
-'What is your email address?', 
-"What is your project's name?", 
-'Please write a small description of your project',
-'What kind of license should your project have?',
-'What command should be run to install dependencies?',
-'What command should be run to run tests?',
-'What does the user need to know about using the repo?',
-'What does the user need to know about contributing to the repo?'
-];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'username',
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email',
+    },
+    {
+        type: 'input',
+        message: "What is your project's name?",
+        name: 'project',
+    },
+    {
+        type: 'input',
+        message: 'Please write a small description of your project',
+        name: 'description',
+    },
+    {
+        type: 'list',
+        message: 'What kind of license should your project have?',
+        name: 'license',
+        choices: ['MIT','APACHE 2.0','GPL 3.0','BSD 3','None'],
+    },
+    {
+        type: 'input',
+        message: 'What command should be run to install dependencies?',
+        name: 'install',
+    },
+    {
+        type: 'input',
+        message: 'What command should be run to run tests?',
+        name: 'test',
+    },
+    {
+        type: 'input',
+        message: 'What does the user need to know about using the repo?',
+        name: 'using',
+    },
+    {
+        type: 'input',
+        message: 'What does the user need to know about contributing to the repo?',
+        name: 'contributing',
+    }
+  
+    ];
 
 
 
@@ -21,55 +61,7 @@ function writeToFile(fileName, data) {
 // function to initialize program
 function init() {
     inquirer
-        .prompt([
-        {
-            type: 'input',
-            message: questions[0],
-            name: 'username',
-        },
-        {
-            type: 'input',
-            message: questions[1],
-            name: 'email',
-        },
-        {
-            type: 'input',
-            message: questions[2],
-            name: 'project',
-        },
-        {
-            type: 'input',
-            message: questions[3],
-            name: 'description',
-        },
-        {
-            type: 'list',
-            message: questions[4],
-            name: 'license',
-            choices: ['MIT','APACHE 2.0','GPL 3.0','BSD 3','None'],
-        },
-        {
-            type: 'input',
-            message: questions[5],
-            name: 'install',
-        },
-        {
-            type: 'input',
-            message: questions[6],
-            name: 'test',
-        },
-        {
-            type: 'input',
-            message: questions[7],
-            name: 'using',
-        },
-        {
-            type: 'input',
-            message: questions[8],
-            name: 'contributing',
-        }
-      
-        ])
+        .prompt(questions)
         .then((response) =>
         response.confirm === response.password
             ? console.log('Success!')
@@ -79,6 +71,7 @@ function init() {
 
 // function call to initialize program
 init();
+
 
 
 
