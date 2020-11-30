610550353079728ae0e8f5c07bbc7c8960739173
Eduardo Coello <jeck24@hotmail.com>
2020-11-30 17:16:08 -0600

    json working

--- index.js
+++ index.js
@@ -6,48 +6,48 @@ const questions = [
     {
         type: 'input',
         message: 'What is your GitHub username?',
-        name: 'username',
+        name: 'GitHub username',
     },
     {
         type: 'input',
         message: 'What is your email address?',
-        name: 'email',
+        name: 'Email address',
     },
     {
         type: 'input',
         message: "What is your project's name?",
-        name: 'project',
+        name: 'Project name',
     },
     {
         type: 'input',
         message: 'Please write a small description of your project',
-        name: 'description',
+        name: 'Project description',
     },
     {
         type: 'list',
         message: 'What kind of license should your project have?',
-        name: 'license',
+        name: 'Project license',
         choices: ['MIT','APACHE 2.0','GPL 3.0','BSD 3','None'],
     },
     {
         type: 'input',
         message: 'What command should be run to install dependencies?',
-        name: 'install',
+        name: 'Command to install',
     },
     {
         type: 'input',
         message: 'What command should be run to run tests?',
-        name: 'test',
+        name: 'Command to test',
     },
     {
         type: 'input',
         message: 'What does the user need to know about using the repo?',
-        name: 'using',
+        name: 'Using the repo',
     },
     {
         type: 'input',
         message: 'What does the user need to know about contributing to the repo?',
-        name: 'contributing',
+        name: 'Contributing to the repo',
     }
   
     ];
@@ -55,23 +55,25 @@ const questions = [
 
 
 // function to write README file
-function writeToFile(fileName, data) {
+function writeToFile(data) {
+    const fileName = `${data.name}.json`;
+    
+    fs.writeFile(fileName, JSON.stringify(data, null, '\t'), (err) =>
+      err ? console.log(err) : console.log('Success!')
+    );
 }
 
 // function to initialize program
 function init() {
     inquirer
         .prompt(questions)
-        .then((response) =>
-        response.confirm === response.password
-            ? console.log('Success!')
-            : console.log('You forgot your password already?!')
+        .then((data) =>
+            writeToFile(data)
         );
 }
 
 // function call to initialize program
 init();
-
 
 
 
